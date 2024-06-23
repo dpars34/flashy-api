642f2da53389b4423bbe394bd68d211e426055b7
Daniel Parsons <dpars@hotmail.co.uk>
2024-06-23 15:42:56 +0900

    login api created

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DeckController;
 
 Route::get('/user', function (Request $request) {
@@ -18,3 +19,15 @@ Route::get('/test', function () {
 // DECKS
 Route::get('/decks', [DeckController::class, 'index']);
 Route::get('/decks/{id}', [DeckController::class, 'show']);
+
+// LOGIN
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+// PROTECTED ROUTES
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+});
